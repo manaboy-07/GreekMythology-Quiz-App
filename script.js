a7baac359c1f4d7d2861afab688e4dfc9c700a88
manaboy-07 <manassehoruebor@gmail.com>
2022-07-14 20:24:09 +0100

    implemented a greek mythology quiz

---
+++ script.js
@@ -0,0 +1,517 @@
+const quizData = [
+    // question 1
+    {
+       question: "In Greek mythology Medusa’s hair was made of what?",
+       a: "Snakes",
+       b: "Cat",
+       c: "Dog",
+       d: "Mouse",
+       correct: "a" ,
+    },
+     // question 2
+     {
+        question: "Which creature in Greek mythology was half-man and half-bull?",
+        a: "Crocodilion",
+        b: "Sphinx",
+        c: "Minotaur",
+        d: "Odessey",
+        correct: "c" ,
+     },
+      // question 3
+    {
+        question: "What was hades pet?",
+        a: "Three headed Lion",
+        b: "Two headed bull",
+        c: "Three headed dog",
+        d: "Five headed cat",
+        correct: "c" ,
+     },
+      // question 4
+    {
+        question: "What did Daedalus construct for King Minos so that he could keep the Minotaur there?",
+        a: "Labyrinth",
+        b: "Grass",
+        c: "Meadow",
+        d: "Maze",
+        correct: "a" ,
+     },
+      // question 5
+    {
+        question: "What was left in Pandora’s box after she released misery and evil?",
+        a: "Sadness",
+        b: "Grief",
+        c: "Joy",
+        d: "Hope",
+        correct: "d" ,
+     },
+      // question 6
+    {
+        question: "According to legend, who fired the arrow that hit Achilles in the heel, his only vulnerable spot?",
+        a: "Hercules",
+        b: "Jerry",
+        c: "James",
+        d: "Paris",
+        correct: "d" ,
+     },
+      // question 7
+    {
+        question: "Who was the ancient Greek God that was both God of the Underworld and God of the Dead?",
+        a: "Grim Ripper",
+        b: "Zeus",
+        c: "Apollo",
+        d: "Hades",
+        correct: "d" ,
+     },
+      // question 8
+    {
+        question: "Who is the goddess of the rainbow",
+        a: "Medusa",
+        b: "iris",
+        c: "Aphrodite",
+        d: "Athena",
+        correct: "b" ,
+     },
+      // question 9
+    {
+        question: "Who is the greek god of the sea ?",
+        a: "Poseidon",
+        b: "Trition",
+        c: "Uranus",
+        d: "Cronus",
+        correct: "a" ,
+     },
+      // question 10
+    {
+        question: "Who spurned Echo’s love and was punished by being made to fall in love with his won reflection?",
+        a: "Scorpio",
+        b: "Ares",
+        c: "Narcissus",
+        d: "Troy",
+        correct: "c" ,
+     },
+      // question 11
+    {
+        question: "what animal is symbolized for the trojan war ?",
+        a: "Horse",
+        b: "Pig",
+        c: "Goat",
+        d: "Bull",
+        correct: "a" ,
+     },
+      // question 12
+    {
+        question: "who is the god of whine ?",
+        a: "Ephritus",
+        b: "Dionysus",
+        c: "Jason",
+        d: "Argos",
+        correct: "b" ,
+     },
+      // question 13
+    {
+        question: "What was the Greek hero Achilles’ only vulnerable part?",
+        a: "Heel",
+        b: "Elbow",
+        c: "Chest",
+        d: "Pelvis",
+        correct: "a" ,
+     },
+      // question 14
+    {
+        question: "How many labours did Hercules face ?",
+        a: "12",
+        b: "11",
+        c: "15",
+        d: "14",
+        correct: "a" ,
+     },
+      // question 15
+    {
+        question: "Amongst his labours Hercules cleaned which stables",
+        a: "Kronos stables",
+        b: "Aregean stables",
+        c: "Hera stables",
+        d: "Meg stables",
+        correct: "b" ,
+     },
+      // question 16
+    {
+        question: "What is the name of the winged horse in Greek mythology?",
+        a: "Felix",
+        b: "Scott",
+        c: "Unicorn",
+        d: "Pegasus",
+        correct: "d" ,
+     },
+      // question 17
+    {
+        question: "who was the goddess of love ?",
+        a: "Eros",
+        b: "Aphrodite",
+        c: "Athena",
+        d: "Hera",
+        correct: "b" ,
+     },
+      // question 18
+    {
+        question: "In Greek mythology, who was the queen of the underworld and wife of Hades?",
+        a: "Persephone",
+        b: "Demeter",
+        c: "Nyx",
+        d: "Hermes",
+        correct: "a" ,
+     },
+      
+    
+      // question 19
+    {
+        question: "Who is known as queen of the gods",
+        a: "Hera",
+        b: "Artemis",
+        c: "Athena",
+        d: "Aphrodite",
+        correct: "a" ,
+     },
+      // question 20
+    {
+      question: "Who is known as goddess of the night",
+      a: "Vixen",
+      b: "Artemis",
+      c: "Nyx",
+      d: "Homer",
+      correct: "c" ,
+   },
+    // question 21
+    {
+      question: "Who is artemis twin",
+      a: "Apollo",
+      b: "Hermes",
+      c: "Ares",
+      d: "Leto",
+      correct: "a" ,
+   },
+    // question 22
+    {
+      question: "what animal was associated with artemis",
+      a: "Eagle",
+      b: "Peacock",
+      c: "Deer",
+      d: "Cat",
+      correct: "c" ,
+   },
+    // question 23
+    {
+      question: "who sent snakes after hercules",
+      a: "Hades",
+      b: "Hera",
+      c: "Medusa",
+      d: "Alex",
+      correct: "b" ,
+   },
+     // question 24
+     {
+      question: "Boreas is the Greek God of what?",
+      a: "Fire",
+      b: "North wind",
+      c: "Snow",
+      d: "Rain",
+      correct: "b" ,
+   },
+    // question 25
+    {
+      question: "In mythology, who was the first (and last) person to solve the Riddle of the Sphinx?",
+      a: "Odeipus",
+      b: "Odessey",
+      c: "Jeremiah",
+      d: "Argos",
+      correct: "a" ,
+   },
+    // question 26
+    {
+      question: "He was the Gods Messenger?",
+      a: "Hera",
+      b: "Hermes",
+      c: "Athena",
+      d: "Apollo",
+      correct: "b" ,
+   },
+    // question 27
+    {
+      question: "Zeus gave birth to Athena through his?",
+      a: "Stomach",
+      b: "Mouth",
+      c: "Skull",
+      d: "Eyes",
+      correct: "c" ,
+   },
+    // question 28
+    {
+      question: "WHO IN GREEK MYTHOLOGY FELL IN LOVE WITH HIS OWN REFLECTION?",
+      a: "Joker",
+      b: "Merlin",
+      c: "Perseaus",
+      d: "Narcissus",
+      correct: "d" ,
+   },
+    // question 29
+    {
+      question: "Who slayed medussa?",
+      a: "Hercules",
+      b: "Perseus",
+      c: "Jason",
+      d: "Ulysses",
+      correct: "b" ,
+   },
+   // question 30
+   {
+      question: "Who slayed Argos ?",
+      a: "Hermes",
+      b: "Hercules",
+      c: "Paris",
+      d: "Poseidon",
+      correct: "a" ,
+   },
+   // question 31
+   {
+      question: "Goddess of the hunt?",
+      a: "Artemis",
+      b: "Demeter",
+      c: "Persephone",
+      d: "Eris",
+      correct: "a" ,
+   },
+   // question 32
+   {
+      question: "Goddess of Chaos",
+      a: "Nyx",
+      b: "Momus",
+      c: "Geras",
+      d: "Eris",
+      correct: "d" ,
+   },
+   // question 33
+   {
+      question: "In greek mythology who was the first woman",
+      a: "Pandora",
+      b: "Eve",
+      c: "Demeter",
+      d: "Mary",
+      correct: "a" ,
+   },
+     // question 34
+     {
+      question: "who did argos serve?",
+      a: "Zeus",
+      b: "Hera",
+      c: "Artemis",
+      d: "Athena",
+      correct: "b" ,
+   },
+     // question 35
+     {
+      question: "Who turned a woman into a spider",
+      a: "Poseidon",
+      b: "Zeus",
+      c: "Athena",
+      d: "Eris",
+      correct: "c" ,
+   },
+     // question 36
+     {
+      question: "Argos was a monster with many eyes , when he was killed his eyes where put on which animal?",
+      a: "Zebra",
+      b: "Wilderbeast",
+      c: "Flying fish",
+      d: "Peacock",
+      correct: "d" ,
+   },
+     // question 37
+     {
+      question: "Athena was associated with which animal?",
+      a: "Owl",
+      b: "Peacock",
+      c: "Eagle",
+      d: "Bull",
+      correct: "a" ,
+   },
+    // question 38
+    {
+      question: "What was the name of the ‘river of hate’ that flowed round the Underworld?",
+      a: "River Nile",
+      b: "River Styx",
+      c: "River of Poseidon",
+      d: "Neptunes wave",
+      correct: "b" ,
+   },
+    // question 39
+    {
+      question: "Who were the guardian spirits of nature in Greek mythology?",
+      a: "Nymphs",
+      b: "Mermaids",
+      c: "Elves",
+      d: "Devils",
+      correct: "a" ,
+   },
+    // question 40
+    {
+      question: "The search of the golden fleece was ordered by who",
+      a: "King Pelias",
+      b: "King Orion",
+      c: "King fleece",
+      d: "King Jason",
+      correct: "a" ,
+   },
+    // question 41
+    {
+      question: "what mountain did the gods live on",
+      a: "Mount everest",
+      b: "Mount Fuji",
+      c: "Mount Kilimanjaro",
+      d: "Mount Olympus",
+      correct: "d" ,
+   },
+    // question 42
+    {
+      question: "WHO WAS THE GREEK GOD OF TRAVELLERS AND THIEVES?",
+      a: "Hermes",
+      b: "Paris",
+      c: "Hercules",
+      d: "Apollo",
+      correct: "a" ,
+   },
+    // question 43
+    {
+      question: "WHO IS THE GREEK GOD OF MUSIC?",
+      a: "Hephaestus",
+      b: "Apollo",
+      c: "Kion",
+      d: "Phillip",
+      correct: "b" ,
+   },
+    // question 44
+    {
+      question: "Who is the god of the forges",
+      a: "Dionysus",
+      b: "Hephaestus",
+      c: "Lamda",
+      d: "Hades",
+      correct: "b" ,
+   },
+    // question 45
+    {
+      question: "who is known as the god of wisdom",
+      a: "Athena",
+      b: "Aphrodite",
+      c: "Nyx",
+      d: "Merida",
+      correct: "" ,
+   },
+    // question 46
+    {
+      question: "who is known as the god of war",
+      a: "Ares",
+      b: "Hermes",
+      c: "Zeus",
+      d: "Apollo",
+      correct: "a" ,
+   },
+    // question 47
+    {
+      question: "WHICH NAME IS SHARED BY THE GREEK GODDESS OF VICTORY & AN INTERNATIONAL SPORTSWARE MANUFACTURER?",
+      a: "Puma",
+      b: "Adidas",
+      c: "Nike",
+      d: "Reebok",
+      correct: "c" ,
+   },
+    // question 48
+    {
+      question: "Who got too close to the sun after disobeying his father",
+      a: "Icarus",
+      b: "Ichigo",
+      c: "Deidara",
+      d: "Daedalus",
+      correct: "a" ,
+   },
+    // question 49
+    {
+      question: "Who overthrew Uranus",
+      a: "Cronus",
+      b: "Zeus",
+      c: "Apollo",
+      d: "Paris of troy",
+      correct: "a" ,
+   },
+    // question 50
+    {
+      question: "Who overthrew Cronus",
+      a: "Hercules",
+      b: "Poseidon",
+      c: "Hades",
+      d: "Zeus",
+      correct: "d" ,
+   },
+   
+    
+    
+    
+]
+
+//Grabbed elements
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz()
+
+function loadQuiz(){
+   const currentQuizData = quizData[currentQuiz];
+   deselectAnswers()
+
+   questionEl.innerText = currentQuizData.question
+   a_text.innerText = currentQuizData.a
+   b_text.innerText = currentQuizData.b
+   c_text.innerText = currentQuizData.c
+   d_text.innerText = currentQuizData.d
+}
+function deselectAnswers(){
+   answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected(){
+   let answer
+
+   answerEls.forEach(answerEl => {
+      if(answerEl.checked){
+         answer = answerEl.id
+      }
+   })
+
+   return answer
+};
+
+submitBtn.addEventListener('click', () => {
+   const answer = getSelected()
+
+   if(answer){
+      if(answer === quizData[currentQuiz].correct){
+         score++
+      }
+       currentQuiz++
+
+         if(currentQuiz < quizData.length){
+         loadQuiz()
+      }   else{
+         quiz.innerHTML = `<h2>You answered  ${score} / ${quizData.length} questions  correctly .</h2>
+         <button onClick = "location.reload()">Reload</button>`
+      }
+   }
+})
